5f9163a200f257947f8460f9563970ae00abfbb3
Darlington <onwuka.xo@gmail.com>
2025-07-21 14:34:30 +0100

    functions

---
+++ JS_project2.js
@@ -0,0 +1,88 @@
+// Instructions
+
+// String Manipulation Functions:
+// Reverse a String: Write a function that reverses a given string.
+// Count Characters: Create a function that counts the number of characters in a string.
+// Capitalize Words: Implement a function that capitalizes the first letter of each word in a sentence.
+
+// Array Functions:
+// Find Maximum and Minimum: Write functions to find the maximum and minimum values in an array of numbers.
+// Sum of Array: Create a function that calculates the sum of all elements in an array.
+// Filter Array: Implement a function that filters out elements from an array based on a given condition.
+
+// Mathematical Functions:
+// Factorial: Write a function to calculate the factorial of a given number.
+// Prime Number Check: Create a function to check if a number is prime or not.
+// Fibonacci Sequence: Implement a function to generate the Fibonacci sequence up to a given number of terms. (search on the net)
+
+// // reverse string function
+
+// function reverseString(str) {
+//     let reversed = " ";
+//     for (let i = str.length - 1; i>= 0; i--) {
+//         reversed += str [i];
+//     }
+//     return reversed;
+// }
+
+// console.log(reverseString("darlington"));
+
+// // Count Characters: Create a function that counts the number of characters in a string.
+
+// function characterCount(str) {
+//    return str.length;
+//     }
+
+// console.log(characterCount("darlington"));
+
+
+// Capitalize Words: Implement a function that capitalizes the first letter of each word in a sentence.
+
+// function capitalFirstLetter(sentence) {
+//   let word = sentence.split(" ");
+//   let capitalLetter = [];
+
+//   for (let i = 0; i < word.length; i++) {
+//     let letter = word[i];
+//     if (letter.length > 0) {
+//       let capital = letter[0].toUpperCase() + letter.slice(1);
+//       capitalLetter.push(capital);
+//     } else {
+//       capitalLetter.push(letter);
+//     }
+// }
+// return capitalLetter.join(" ");
+// }
+
+
+// console.log(capitalFirstLetter('darlington is a very good boy'));
+
+
+// Find Maximum and Minimum: Write functions to find the maximum and minimum values in an array of numbers.
+
+//     function findMinMax(numbers) {
+//     return [Math.min(...numbers), Math.max(...numbers)];
+// }
+ 
+//     console.log(findMinMax([15, 2, 5, 99, 0]));
+
+
+// Sum of Array: Create a function that calculates the sum of all elements in an array.
+// function arraySum(arr) {
+//     let sum = 0;
+
+//     for (let i = 0; i < arr.length; i++) {     
+//          sum += arr[i];
+//     }
+//     return sum;
+// }
+
+// console.log(arraySum([200, 5, 10]));
+
+
+
+// Filter Array: Implement a function that filters out elements from an array based on a given condition.
+function filter(arr) {
+    ret
+}
+
